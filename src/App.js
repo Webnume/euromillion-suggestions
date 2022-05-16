20d8b7c61a1425eb0b8797920cc52f3ac5195464
Yves G <contact@webnumerik.fr>
2022-05-17 01:14:32 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    fetch(`http://localhost:3000/`, {
+      method: "GET",
+      headers: new Headers(),
+      mode: "cors",
+      cache: "default",
+    }).then((response) =>
+      response
+        .json()
+        .then((result) => {
+          console.log(result);
+          setData(result);
+          setLoading(false);
+        })
+        .catch((error) => {console.log(error);
+          setError(error);
+          setLoading(false);
+        })
+    );
+  }, []);
+
   return (
+     loading ? "loading" :
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data[0].map((element, index) => (
+        <div
+          key={index}
+        >{`${element.numero} : ${element.nombreDeSorties}`}</div>
+      ))}
+      {data[1].map((element, index) => (
+        <div
+          key={index}
+        >{`${element.numeroStars} : ${element.nombreDeSortiesStars}`}</div>
+      ))}
     </div>
   );
 }

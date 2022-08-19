d2cceda7af77e025bfaefabc61de2844dca5ed76
Yves G <contact@webnumerik.fr>
2022-08-20 00:52:05 +0200

    fix:

--- src/components/EuroMillion/EuroMillion.js
+++ src/components/EuroMillion/EuroMillion.js
@@ -41,7 +41,7 @@ function EuroMillion({ data }) {
           chance ! Jouez raisonnablement svp.
         </p>
         <p className="description">
-          <b>[ENG]</b> This app is made for you if you need of inspiration to
+          <b>[ENG]</b> This app is made for you if you need inspiration to
           choose your Euromillions numbers. The algorithm is based on the output
           probabilities of the numbers. These numbers are updated automatically
           with each result twice a week. Good luck ! Please play reasonably.
